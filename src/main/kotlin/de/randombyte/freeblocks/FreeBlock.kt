b89b2f3da3c61f632598cc162122d95cf2d21959
randombyte-developer <apps.randombyte@gmail.com>
2016-07-24 00:44:11 +0200

    Some test functionality

---
+++ src/main/kotlin/de/randombyte/freeblocks/FreeBlock.kt
@@ -0,0 +1,78 @@
+package de.randombyte.freeblocks
+
+import org.spongepowered.api.Sponge
+import org.spongepowered.api.block.BlockState
+import org.spongepowered.api.block.BlockTypes
+import org.spongepowered.api.data.key.Keys
+import org.spongepowered.api.data.manipulator.mutable.PotionEffectData
+import org.spongepowered.api.effect.potion.PotionEffect
+import org.spongepowered.api.effect.potion.PotionEffectTypes
+import org.spongepowered.api.entity.Entity
+import org.spongepowered.api.entity.EntityType
+import org.spongepowered.api.entity.EntityTypes
+import org.spongepowered.api.event.cause.Cause
+import org.spongepowered.api.world.Location
+import org.spongepowered.api.world.extent.Extent
+
+class FreeBlock private constructor(val location: Location<out Extent>, val armorStand: Entity, val fallingBlock: Entity, val shulker: Entity) {
+    companion object {
+        lateinit var spawnCause: Cause
+
+        fun init(spawnCause: Cause, pluginInstance: Any) {
+            this.spawnCause = spawnCause
+            Sponge.getScheduler().createTaskBuilder().execute { ->
+                resetFallTime() // Prevents despawning
+            }.intervalTicks(1).submit(pluginInstance)
+        }
+
+        fun create(location: Location<out Extent>, blockState: BlockState): FreeBlock {
+            if (location.hasBlock()) location.block = BlockTypes.AIR.defaultState
+            val armorStand = spawnArmorStand(location)
+            val fallingBlock = spawnFallingBlock(location, blockState)
+            val shulker = spawnShulker(location)
+            armorStand.addPassenger(fallingBlock)
+            armorStand.addPassenger(shulker)
+            return FreeBlock(location, armorStand, fallingBlock, shulker)
+        }
+
+        private fun spawnEntity(location: Location<out Extent>, entityType: EntityType,
+                                modifyEntityFunc: (Entity) -> Unit): Entity {
+            val entity = location.extent.createEntity(entityType, location.position).orElseThrow {
+                RuntimeException("Couldn't create ${entityType.name} at $location!")
+            }
+            modifyEntityFunc.invoke(entity)
+            if (!location.extent.spawnEntity(entity, spawnCause))
+                throw RuntimeException("Couldn't spawn ${entityType.name} at $location!")
+            return entity
+        }
+
+        private fun spawnArmorStand(location: Location<out Extent>) = spawnEntity(location, EntityTypes.ARMOR_STAND, {
+            it.offer(Keys.INVISIBLE, true)
+            it.offer(Keys.ARMOR_STAND_HAS_GRAVITY, false)
+            it.offer(Keys.ARMOR_STAND_MARKER, true)
+        })
+
+        private fun spawnFallingBlock(location: Location<out Extent>, blockState: BlockState) = spawnEntity(location, EntityTypes.FALLING_BLOCK, {
+            it.offer(Keys.FALL_TIME, Integer.MIN_VALUE)
+            it.offer(Keys.FALLING_BLOCK_STATE, blockState)
+        })
+
+        private fun spawnShulker(location: Location<out Extent>) = spawnEntity(location, EntityTypes.SHULKER, {
+            it.offer(Keys.AI_ENABLED, false)
+            it.offer(Keys.IS_SILENT, true)
+            it.offer(Keys.PERSISTS, true)
+            it.offer(it.getOrCreate(PotionEffectData::class.java).get().effects()
+                    .add(PotionEffect.of(PotionEffectTypes.INVISIBILITY, 0, Integer.MAX_VALUE)))
+        })
+
+        private fun resetFallTime() {
+            Sponge.getServer().worlds.forEach { world ->
+                world.loadedChunks.forEach { chunk ->
+                    chunk.entities.filter { it.type.equals(EntityTypes.FALLING_BLOCK) }.forEach { fallingBlock ->
+                        fallingBlock.offer(Keys.FALL_TIME, 1)
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/de/randombyte/freeblocks/FreeBlocks.kt
+++ src/main/kotlin/de/randombyte/freeblocks/FreeBlocks.kt
@@ -4,9 +4,21 @@ import com.google.inject.Inject
 import ninja.leaping.configurate.commented.CommentedConfigurationNode
 import ninja.leaping.configurate.loader.ConfigurationLoader
 import org.slf4j.Logger
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.config.DefaultConfig
+import org.spongepowered.api.data.key.Keys
+import org.spongepowered.api.data.property.block.SolidCubeProperty
+import org.spongepowered.api.data.type.HandTypes
+import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.block.InteractBlockEvent
+import org.spongepowered.api.event.cause.Cause
+import org.spongepowered.api.event.cause.NamedCause
+import org.spongepowered.api.event.cause.entity.spawn.SpawnCause
+import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes
+import org.spongepowered.api.event.filter.cause.First
 import org.spongepowered.api.event.game.state.GameInitializationEvent
+import org.spongepowered.api.item.ItemTypes
 import org.spongepowered.api.plugin.Plugin
 
 @Plugin(id = FreeBlocks.ID, name = FreeBlocks.NAME, version = FreeBlocks.VERSION, authors = arrayOf(FreeBlocks.AUTHOR))
@@ -18,8 +30,31 @@ class FreeBlocks @Inject constructor(val logger: Logger, @DefaultConfig(sharedRo
         const val AUTHOR = "RandomByte"
     }
 
+    private val selectedFreeBlocks = mutableListOf<FreeBlock>()
+
     @Listener
     fun onInit(event: GameInitializationEvent) {
+        FreeBlock.init(Cause.of(NamedCause.source(SpawnCause.builder().type(SpawnTypes.PLUGIN).build()),
+                NamedCause.owner(Sponge.getPluginManager().fromInstance(this).get())), this)
+
         logger.info("$NAME loaded: $VERSION")
+    }
+
+    @Listener
+    fun featherRightClick(event: InteractBlockEvent.Secondary, @First player: Player) {
+        if (player.getItemInHand(HandTypes.MAIN_HAND).orElse(null)?.item?.equals(ItemTypes.FEATHER) ?: false) {
+            if (player.getOrElse(Keys.IS_SNEAKING, false)) {
+                // Switch block movement direction
+            } else {
+                // Select block
+                if (event.targetBlock.getProperty(SolidCubeProperty::class.java).orElse(null)?.value ?: false) {
+                    // Is representable by a FallingBlock and has the same hitbox as a shulker
+                    val targetLocation = event.targetBlock.location.orElseThrow {
+                        RuntimeException("Couldn't get location of block that was right clicked!")
+                    }
+                    val freeBlock = FreeBlock.create(targetLocation.add(0.0, 0.3, 0.0), targetLocation.block)
+                }
+            }
+        }
     }
 }
